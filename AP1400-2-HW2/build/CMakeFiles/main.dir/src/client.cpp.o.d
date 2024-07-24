cd07e424a7546eb5b80643920bf8e7b530c72bbc
bduoluoluo <1036575651@qq.com>
2024-07-24 17:40:00 +0800

    HW2 init!

---
+++ AP1400-2-HW2/build/CMakeFiles/main.dir/src/client.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/src/client.cpp.o: \
+ /root/AP1400-2/AP1400-2-HW2/src/client.cpp /usr/include/stdc-predef.h \
+ /root/AP1400-2/AP1400-2-HW2/include/client.h
---
+++ AP1400-2-HW2/build/CMakeFiles/main.dir/src/server.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/src/server.cpp.o: \
+ /root/AP1400-2/AP1400-2-HW2/src/server.cpp /usr/include/stdc-predef.h \
+ /root/AP1400-2/AP1400-2-HW2/include/server.h
